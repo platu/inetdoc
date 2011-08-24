a5331284bb839584956b9531037684331a11eba5
Philippe Latu <philippe.latu@inetdoc.net>
2011-08-24 11:15:58 +0200

    Ajout de guides supplémentaires + bibliographie

---
+++ guides/iptables-tutorial/iptables-tutorial-sgml2xmldb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for file in `find /home/phil/src/home/mb/traduc/appendices -type f -name "*.sgml"`; do 
+  echo `basename $file .sgml`
+  iconv -f iso-8859-1 -t utf-8 $file -o appendices/`basename $file .sgml`.xml
+done
+
+for file in `find /home/phil/src/home/mb/traduc/chapters -type f -name "*.sgml"`; do 
+  echo `basename $file .sgml`
+  iconv -f iso-8859-1 -t utf-8 $file -o chapters/`basename $file .sgml`.xml
+done
+
+for file in `find /home/phil/src/home/mb/traduc/licensing -type f -name "*.sgml"`; do 
+  echo `basename $file .sgml`
+  iconv -f iso-8859-1 -t utf-8 $file -o licensing/`basename $file .sgml`.xml
+done
+
+for file in `find /home/phil/src/home/mb/traduc/images -type f -name "*.jpg"`; do 
+  echo `basename $file .jpg`
+  cp $file images/
+  mogrify -format png images/`basename $file`
+done
+
+rm images/*.jpg
+
