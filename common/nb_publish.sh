2b339f681f9c1c456113f88aed5b51966598311a
Philippe Latu <philippe.latu@inetdoc.net>
2011-09-14 22:58:41 +0200

    Google analytics + migration OSPF

--- common/nb_publish.sh
+++ common/nb_publish.sh
@@ -2,6 +2,6 @@
 
 NB_DEV=/home/phil/inetdoc
 
-rsync -avh --delete --links --no-checksum \
+rsync -avh --delete --copy-unsafe-links --no-checksum \
       --exclude-from=$NB_DEV/common/nb_rsync_exclude.txt \
       $NB_DEV/* /var/www/
