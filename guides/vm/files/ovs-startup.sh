842cbc0c12c2a88e121eb5f74aca6a96c4356aac
Philippe Latu <philippe.latu@inetdoc.net>
2015-10-21 00:13:36 +0200

    Corrections multiples routage inter-VLAN
    . Version intermédiaire avec configuraton du commutateur du système hôte
    . Préparation fichier système /etc/network/interfaces équivalent aux
      manipulations manuelles
    . Édition script de lancement de machine virtuelle avec Open vSwitch
    .

--- guides/vm/files/ovs-startup.sh
+++ guides/vm/files/ovs-startup.sh
@@ -11,19 +11,23 @@ shift
 
 if [[ -z "$vm" || -z "$memory" || -z "$tapnum" ]]
 then
-    echo "ERREUR : paramètre manquant"
-    echo "Utilisation : $0 <fichier image> <quantité mémoire en Mo> <numéro interface tap>"
-    exit 1
+	echo "ERREUR : paramètre manquant"
+	echo "Utilisation : $0 <fichier image> <quantité mémoire en Mo> <numéro interface tap>"
+	exit 1
 fi
 
 if (( $memory < 128 ))
 then
-    echo "ERREUR : quantité de mémoire RAM insuffisante"
-    echo "La quantité de mémoire en Mo doit être supérieure ou égale à 128"
-    exit 1
+	echo "ERREUR : quantité de mémoire RAM insuffisante"
+	echo "La quantité de mémoire en Mo doit être supérieure ou égale à 128"
+	exit 1
 fi
 
-macaddress="ba:ad:00:ca:fe:`printf "%02x" $tapnum`"
+second_rightmost_byte=$(printf "%02x" $(expr $tapnum / 256))
+rightmost_byte=$(printf "%02x" $(expr $tapnum % 256))
+macaddress="ba:ad:ca:fe:$second_rightmost_byte:$rightmost_byte"
+
+image_format="${vm##*.}"
 
 echo -e "$RedOnBlack"
 echo "~> Machine virtuelle : $vm"
@@ -33,26 +37,26 @@ echo "~> Adresse MAC       : $macaddress"
 tput sgr0
 
 ionice -c3 qemu-system-x86_64 \
-  -machine type=q35,accel=kvm:tcg \
-  -cpu qemu64,+ssse3,+sse4.1,+sse4.2,+x2apic \
-  -daemonize \
-  -name $vm \
-  -m $memory \
-  -balloon virtio \
-  -smp 2,cores=2 \
-  -rtc base=localtime,clock=host \
-  -boot order=c \
-  -drive if=none,id=drive0,aio=native,format=raw,media=disk,file=$vm \
-  -device virtio-blk,drive=drive0,scsi=off,config-wce=off,x-data-plane=on \
-  -k fr \
-  -vga qxl \
-  -spice port=$((5900 + $tapnum)),addr=localhost,disable-ticketing \
-  -device virtio-serial-pci \
-  -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
-  -chardev spicevmc,id=spicechannel0,name=vdagent \
-  -usb -usbdevice tablet \
-  -soundhw hda \
-  -device virtio-net,netdev=net0,mac="$macaddress" \
-  -netdev tap,ifname=tap$tapnum,id=net0,script=no \
-  $*
+	-machine type=q35,accel=kvm:tcg \
+	-cpu qemu64,+ssse3,+sse4.1,+sse4.2,+x2apic \
+	-daemonize \
+	-name $vm \
+	-m $memory \
+	-balloon virtio \
+	-smp 2,cores=2 \
+	-rtc base=localtime,clock=host \
+	-boot order=c \
+	-drive if=none,id=drive0,aio=native,cache=directsync,format=$image_format,media=disk,file=$vm \
+	-device virtio-blk,drive=drive0,scsi=off,config-wce=off,x-data-plane=on \
+	-k fr \
+	-vga qxl \
+	-spice port=$((5900 + $tapnum)),addr=localhost,disable-ticketing \
+	-device virtio-serial-pci \
+	-device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
+	-chardev spicevmc,id=spicechannel0,name=vdagent \
+	-usb -usbdevice tablet \
+	-soundhw hda \
+	-device virtio-net,netdev=net0,mac="$macaddress" \
+	-netdev tap,ifname=tap$tapnum,id=net0,script=no \
+	$*
 
