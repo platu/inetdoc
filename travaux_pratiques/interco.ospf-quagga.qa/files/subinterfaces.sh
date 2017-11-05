978463e57b4199d9877292655c8e2fad7d38c8d3
Philippe Latu <philippe.latu@inetdoc.net>
2017-11-05 17:08:23 +0100

    Support TP inter-VLAN & OSPF avec Bird
    . Version initiale du corrigé avec Bird
    . Arborescence et Makefiles
    . Corrections de toutes les anciennes URLs pointant vers le corrigé avec
    Quagga

--- travaux_pratiques/interco.ospf-quagga.qa/files/subinterfaces.sh
+++ travaux_pratiques/interco.ospf-quagga.qa/files/subinterfaces.sh
@@ -3,7 +3,5 @@
 for vlan in $*
 do
 	ip link add link eth0 name eth0.$vlan type vlan id $vlan
-	ip link set dev eth0.$vlan txqueuelen 10000
-	tc qdisc add dev eth0.$vlan root pfifo_fast
 	ip link set dev eth0.$vlan up
 done
