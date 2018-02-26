1b8576c80f893c826e936fd69eac616955edc6b0
Philippe Latu <philippe.latu@inetdoc.net>
2018-02-26 16:33:20 +0100

    Guide virtualisation système et enseignement
    . Révision du script ovs-startup.sh : redéfinition du CPU avec max et
    cache de niveau 3, ajout intel-iommu et utilisation des files multiples
    aux niveauxstockage et réseau
    
    . Révision section tuntap

--- guides/vm/files/ovs-startup.sh
+++ guides/vm/files/ovs-startup.sh
@@ -38,18 +38,20 @@ tput sgr0
 
 ionice -c3 qemu-system-x86_64 \
 	-machine type=q35,accel=kvm:tcg \
-	-cpu qemu64,+ssse3,+sse4.1,+sse4.2,+x2apic \
+	-cpu max,l3-cache=on \
+	-device intel-iommu \
 	-daemonize \
 	-name $vm \
 	-m $memory \
 	-balloon virtio \
-	-smp 2,cores=2 \
+	-smp 2,threads=2 \
 	-rtc base=localtime,clock=host \
 	-watchdog i6300esb \
 	-watchdog-action none \
 	-boot order=c,menu=on \
-	-drive if=none,id=drive0,aio=native,cache.direct=on,format=$image_format,media=disk,file=$vm \
-	-device virtio-blk,drive=drive0,scsi=off,config-wce=off \
+	-object "iothread,id=iothread.drive0" \
+	-drive if=none,id=drive0,aio=native,cache.direct=on,discard=unmap,format=$image_format,media=disk,file=$vm \
+	-device virtio-blk-pci,num-queues=4,drive=drive0,scsi=off,config-wce=off,iothread=iothread.drive0 \
 	-k fr \
 	-vga qxl \
 	-spice port=$((5900 + $tapnum)),addr=localhost,disable-ticketing \
@@ -57,9 +59,9 @@ ionice -c3 qemu-system-x86_64 \
 	-device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
 	-chardev spicevmc,id=spicechannel0,name=vdagent \
 	-usb \
-	-device usb-tablet,bus=usb-bus.0 \
+        -device usb-tablet,bus=usb-bus.0 \
 	-soundhw hda \
-	-device virtio-net,netdev=net0,mac="$macaddress" \
-	-netdev tap,ifname=tap$tapnum,id=net0,script=no \
+	-device virtio-net-pci,mq=on,vectors=6,netdev=net0,mac="$macaddress" \
+	-netdev tap,queues=2,ifname=tap$tapnum,id=net0,script=no,downscript=no,vhost=on \
 	$*
 
