9af381217c02481cb86fe486a60e23d09714aaf5
Philippe Latu <philippe.latu@inetdoc.net>
2018-10-18 19:29:35 +0200

    Support TP OSPF avec Quagga
    . Section manipulations sur VMs
    simplification des scripts et adaptation au tableau du plan d'adressage

--- travaux_pratiques/interco.ospf-quagga.qa/files/patchcables.sh
+++ travaux_pratiques/interco.ospf-quagga.qa/files/patchcables.sh
@@ -2,9 +2,9 @@
 
 for patchtap in $*
 do
-        if [ -z "$(ip addr ls | grep $patchtap)" ]; then
+        if [ -z "$(ip link ls | grep $patchtap)" ]; then
                 echo setting $patchtap up
-                sudo ip tuntap add mode tap dev $patchtap group kvm
+                sudo ip tuntap add mode tap dev $patchtap group kvm multi_queue
                 sudo ip link set dev $patchtap up
         else
                 echo $patchtap is already there!
