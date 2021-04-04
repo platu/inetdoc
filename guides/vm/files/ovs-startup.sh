2f0188aa9d5586f8e9d559521dc2cf4a9d50687d
Philippe Latu <philippe.latu@inetdoc.net>
2021-04-04 16:04:45 +0200

    Révision des scripts de lancement VM + nexus 9000v

--- guides/vm/files/ovs-startup.sh
+++ guides/vm/files/ovs-startup.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-RedOnBlack='\E[31m'
+# This script is part of https://inetdoc.net project
+# 
+# It starts a qemu/kvm x86 virtual machine plugged into an Open VSwitch port
+# through an already existing tap interface.
+# It should be run by a normal user account which belongs to the kvm system
+# group and is able to run the ovs-vsctl command via sudo
+#
+# File: ovs-startup.sh
+# Author: Philippe Latu
+# Source: https://github.com/platu/inetdoc/blob/master/guides/vm/files/ovs-startup.sh
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+RED='\e[5;31m'
+GREEN='\e[5;32m'
+BLUE='\e[5;34m'
+NC='\e[0m' # No Color
 
 vm=$1
 shift
@@ -9,51 +36,61 @@ shift
 tapnum=$1
 shift
 
-# Est-ce que les 3 paramètres sont là ?
+# Are the 3 parameters there ?
 if [[ -z "$vm" || -z "$memory" || -z "$tapnum" ]]
 then
-	echo "ERREUR : paramètre manquant"
-	echo "Utilisation : $0 [fichier image] [quantité mémoire en Mo] [numéro interface tap]"
+	echo -e "${RED}ERROR : missing parameter.${NC}"
+	echo -e "${GREEN}Usage : $0 [image file] [RAM size in MB] [tap interface number]${NC}"
 	exit 1
 fi
 
-# Est-ce que le fichier image existe ?
+# Does the VM image file exist ?
 if [[ ! -f "$vm" ]]
 then
-	echo "ERREUR : Le fichier image $vm n'existe pas !"
+	echo -e "${RED}ERROR : the $vm image file does not exist.${NC}"
 	exit 1
 fi
 
-# Est-ce que la quantité de RAM est suffisante ?
+# Is the amount of ram sufficient to run the VM ?
 if [[ ${memory} -lt 128 ]]
 then
-	echo "ERREUR : mémoire RAM insuffisante : ${memory}Mo"
-	echo "La taille mémoire doit être supérieure ou égale à 128Mo."
+	echo -e "${RED}ERROR : unsifficient RAM size : ${memory}MB${NC}"
+	echo -e "${GREEN}RAM size must be above 128MB.${NC}"
 	exit 1
 fi
 
-# Est-ce que l'interface tap est libre ?
-if [[ ! -z "$(ps aux | grep =[tap]${tapnum}, )" ]]
+# Is the tap interface free ?
+if [[ ! -z "$(ps aux | grep =[t]ap${tapnum}, )" ]]
 then
-	echo "L'interface tap${tapnum} est déjà utilisée !"
+	echo -e "${RED}tap${tapnum} is already in use by another process.${NC}"
 	exit 1
 fi
 
 second_rightmost_byte=$(printf "%02x" $(expr $tapnum / 256))
 rightmost_byte=$(printf "%02x" $(expr $tapnum % 256))
-macaddress="ba:ad:ca:fe:$second_rightmost_byte:$rightmost_byte"
+macaddress="b8:ad:ca:fe:$second_rightmost_byte:$rightmost_byte"
+lladdress="fe80::baad:caff:fefe:$(printf "%x" $tapnum)"
+vlan_mode="$(sudo ovs-vsctl list port tap${tapnum} | grep vlan_mode | egrep -o '(access|trunk)')"
+
+if [[ "$vlan_mode" == "access" ]]
+then
+	svi="vlan$(sudo ovs-vsctl list port tap${tapnum} | grep tag | grep -o -E '[0-9]+')"
+else
+	svi="dsw-host"
+fi
 
 image_format="${vm##*.}"
 
 spice=$((5900 + $tapnum))
 telnet=$((2300 + $tapnum))
 
-echo -e "$RedOnBlack"
-echo "~> Machine virtuelle : $vm"
-echo "~> Port SPICE        : $spice"
-echo "~> Port Console TCP  : $telnet"
-echo "~> Mémoire RAM       : $memory"
-echo "~> Adresse MAC       : $macaddress"
+echo -e "~> Virtual machine filename   : ${RED}${vm}${NC}"
+echo -e "~> RAM size                   : ${RED}${memory}MB${NC}"
+echo -e "~> SPICE VDI port number      : ${GREEN}${spice}${NC}"
+echo -e "~> telnet console port number : ${GREEN}${telnet}${NC}"
+echo -e "~> MAC address                : ${BLUE}${macaddress}${NC}"
+echo -e "~> Switch port interface      : ${BLUE}tap${tapnum}, ${vlan_mode} mode${NC}"
+echo -e "~> IPv6 LL address            : ${BLUE}${lladdress}%${svi}${NC}"
 tput sgr0
 
 ionice -c3 qemu-system-x86_64 \
@@ -86,3 +123,4 @@ ionice -c3 qemu-system-x86_64 \
 	-device virtio-net-pci,mq=on,vectors=6,netdev=net$tapnum,mac="$macaddress" \
 	-netdev tap,queues=2,ifname=tap$tapnum,id=net$tapnum,script=no,downscript=no,vhost=on \
 	$*
+
