6bdc204d7e36f65a7336cc4692187434923ac84e
Philippe Latu <philippe.latu@inetdoc.net>
2024-09-25 17:07:14 +0200

    Travaux pratiques : nettoyages URLs PPPoE

--- articles/pppoe/scripts/pppoe-startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-../scripts/ovs-startup.sh hub.qed 1024 0 \
-        -device virtio-net,netdev=net2,mac=de:ad:be:ef:00:01 \
-        -netdev tap,ifname=tap1,id=net2,script=no
-
-../scripts/ovs-startup.sh spoke.qed 1024 2 \
-        -device virtio-net,netdev=net2,mac=de:ad:be:ef:00:03 \
-        -netdev tap,ifname=tap3,id=net2,script=no
