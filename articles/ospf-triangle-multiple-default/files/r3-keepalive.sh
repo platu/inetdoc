26fccf3854e2e81f2aa332a827887a69f00adec7
Philippe Latu <philippe.latu@inetdoc.net>
2025-08-20 21:00:54 +0200

    Formatage et optimisations PNG

--- articles/ospf-triangle-multiple-default/files/r3-keepalive.sh
+++ articles/ospf-triangle-multiple-default/files/r3-keepalive.sh
@@ -5,10 +5,10 @@
 
 neighbor=10.1.30.10
 
-scriptname=`basename $0`
+scriptname=$(basename $0)
 scriptname=${scriptname%.*}
-delay=10 # sleep time
-logfile=/var/log/$scriptname.log  # file for debug messages
+delay=10                            # sleep time
+logfile=/var/log/$scriptname.log    # file for debug messages
 lockfile=/var/lock/$scriptname.lock # locking to avoid races
 
 # control where to log debug messages to:
@@ -19,71 +19,71 @@ debugfile=1
 # log a message to the log-file specified by debugfile
 function keepalive_debug() {
 
-  local msg="$1"
-  local dat=$(date "+%b %d %H:%M:%S") # time stamp
+	local msg="$1"
+	local dat=$(date "+%b %d %H:%M:%S") # time stamp
 
-  if [ "${debugfile}" == "1" ]; then
-    lockfile -r 1 ${lockfile}
-    if [ "$?" == "0" ]; then
-      touch ${logfile} >/dev/null 2>&1
-      chmod 0644 ${logfile} >/dev/null 2>&1
-      echo -n "${dat}: " >>${logfile} 2>&1
-      echo "${msg}" >>${logfile} 2>&1
-      rm -f ${lockfile} >/dev/null 2>&1
-    fi
-  else
-    echo "${msg}" >/dev/null 2>&1
-  fi
+	if [ "${debugfile}" == "1" ]; then
+		lockfile -r 1 ${lockfile}
+		if [ "$?" == "0" ]; then
+			touch ${logfile} >/dev/null 2>&1
+			chmod 0644 ${logfile} >/dev/null 2>&1
+			echo -n "${dat}: " >>${logfile} 2>&1
+			echo "${msg}" >>${logfile} 2>&1
+			rm -f ${lockfile} >/dev/null 2>&1
+		fi
+	else
+		echo "${msg}" >/dev/null 2>&1
+	fi
 }
 
 # redirect messages from stdin to debug
 function redirect() {
 
-  local msg="$(cat -)"
+	local msg="$(cat -)"
 
-  keepalive_debug "${msg}"
+	keepalive_debug "${msg}"
 }
 
 # ping the ${neighbor} and sleep ${delay} seconds
 function alive_ping() {
 
-  local msg=""
+	local msg=""
 
-  keepalive_debug "alive_ping"
-  while $(true) ; do
-    msg=$(/bin/ping -W 2 -n -c 1 ${neighbor} 2>&1 | egrep '(bytes from|100% packet loss)')
+	keepalive_debug "alive_ping"
+	while $(true); do
+		msg=$(/bin/ping -W 2 -n -c 1 ${neighbor} 2>&1 | egrep '(bytes from|100% packet loss)')
 
-    if [[ "${msg}" =~ "bytes from" ]] && [[ ! -e /tmp/${neighbor}_up ]]; then
-      ip route add default via ${neighbor}
-      touch /tmp/${neighbor}_up
-      keepalive_debug "${neighbor} is reachable, default route added"
-    fi
+		if [[ ${msg} =~ "bytes from" ]] && [[ ! -e /tmp/${neighbor}_up ]]; then
+			ip route add default via ${neighbor}
+			touch /tmp/${neighbor}_up
+			keepalive_debug "${neighbor} is reachable, default route added"
+		fi
 
-    if [[ "${msg}" =~ "100% packet loss" ]] && [[ -e /tmp/${neighbor}_up ]]; then
-      ip route del default via ${neighbor}
-      rm -f /tmp/${neighbor}_up
-      keepalive_debug "${neighbor} is not reachable, default route deleted"
-    fi
+		if [[ ${msg} =~ "100% packet loss" ]] && [[ -e /tmp/${neighbor}_up ]]; then
+			ip route del default via ${neighbor}
+			rm -f /tmp/${neighbor}_up
+			keepalive_debug "${neighbor} is not reachable, default route deleted"
+		fi
 
-    sleep ${delay}
-  done
+		sleep ${delay}
+	done
 }
 
 # start up and handle proper daemonization
 function main() {
 
-  local argument="daemon"
+	local argument="daemon"
 
-  if [ "$1" == "${argument}" ]; then
-    alive_ping
-  else
-    keepalive_debug "START"
-    keepalive_debug "daemonize $0"
-    nohup /bin/bash $0 ${argument} $* >/dev/null 2>&1 &
-    keepalive_debug "daemonized $0"
-  fi
+	if [ "$1" == "${argument}" ]; then
+		alive_ping
+	else
+		keepalive_debug "START"
+		keepalive_debug "daemonize $0"
+		nohup /bin/bash $0 ${argument} $* >/dev/null 2>&1 &
+		keepalive_debug "daemonized $0"
+	fi
 }
 
 main $*
 
-exit 0 
+exit 0
